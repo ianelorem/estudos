c9b0395304489975c62f86f83e2386f9f2628346
Iane Lorem <ianne.lorem17@gmail.com>
2025-03-27 19:02:53 -0300

    questão 1 do dia 27/03 if e else

--- 26_03_25/questao1.js
+++ 26_03_25/questao1.js
@@ -1,1 +1,6 @@
 console.log ("iane")
+
+const a = 5
+const b = 2
+const c = a+b
+console.log (c)
---
+++ 27_03_25/questao1.js
@@ -0,0 +1,11 @@
+const idade = 65
+const sexo = "masculino"
+if (idade >= 65 && sexo=="masculino") {
+    console.log ("é aposentado e é homem" )
+}else {
+    if (idade >= 55 && sexo == "feminino"){
+
+     console.log ("é aposentado e é mulher")
+} else {
+    console.log ("não é aposentado")
+}}

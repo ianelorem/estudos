ed8b597052c174134f23dc7fd60477a638b96df3
Iane Lorem <ianne.lorem17@gmail.com>
2025-03-28 15:13:27 -0300

    questão variavel salario /diferentes saidas no terminal

---
+++ 28_03_25/questao1.js
@@ -0,0 +1,17 @@
+let salario = 3000
+
+if (salario<=280){
+    console.log ("aumento de 20 %")
+
+} 
+if (salario > 280 && salario <= 700){
+    console.log ("aumento de 15 %")
+
+}
+if (salario > 700 && salario <= 1500){
+    console.log ("aumento de 10 %")
+
+}
+if (salario > 1500 ){
+    console.log ("aumento de 5 %")
+}

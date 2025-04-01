f2b7b8f65c0f3216282c8671e340ad4c7cc8fe03
Iane Lorem <ianne.lorem17@gmail.com>
2025-04-01 16:47:55 -0300

    exercios switch e funções

---
+++ 31_03_25/questao1.js
@@ -0,0 +1,27 @@
+const regente = "pastor"
+
+switch (regente){
+    case "pastor":{
+        console.log("o regente é o pastor")
+        break
+    }
+    case "davi": {
+        console.log ("o regente é o davi")
+        break
+    }
+    case "debora":{
+        console.log ("a regente é a debora")
+        break
+    }
+    case "anderson":{
+        console.log ("o regente é o anderson")
+        break
+    }
+    case "yasmin":{
+        console.log ("a regente é a yasmin")
+        break
+    }
+    default: {
+        console.log ("não é nenhum dos anteriores")
+
+    } }
---
+++ 31_03_25/questao2.js
@@ -0,0 +1,11 @@
+
+function mensagemdesalmos (nome){
+    console.log (`${nome}, O senhor o sustentará no leito da doença ; tu o restaurarás de sua cama de enfermidade.`)
+}
+
+mensagemdesalmos ('anderson')
+mensagemdesalmos ('iane')
+mensagemdesalmos ('debora')
+mensagemdesalmos ('davi')
+
+

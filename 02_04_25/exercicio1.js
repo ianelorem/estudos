950a6f5d03a79a3e86adfe80d41fdbead6aba994
Iane Lorem <ianne.lorem17@gmail.com>
2025-04-02 22:52:06 -0300

    teste  que quase deus ruim kk
    Co-authored-by: Anderson Ferreira Alves <AndersonFerreiraAlves007@users.noreply.github.com>

---
+++ 02_04_25/exercicio1.js
@@ -0,0 +1,185 @@
+// vc esta vendo o que eu estou escrevendo
+//sim, ok kkkkk
+// foi mal, perdi a cabeça
+// eu tbm então fica de boa ai
+//por um lado foi bom, agora agente aprendeu esse jeito de estudar
+//so vamos ficar mais um tempinho, pq acabamos perdendo tempo, ok?
+//ok
+
+// aqui estamos criando uma funcção, certo? sim
+// quantos parametros essa função tem? 1
+// qunatos parametros tem? nenhum, ok correto 
+
+// eu pergunto qual o valor que vai pra variavel ola?1 e 2
+// presta atenção, a variavel ola vai ficar com qualq valor? ah não sei 
+ //bora la, a variavel ola vai fica com o retorda da chamada da função, concorda?sim
+ // agora me diz o valor da variavel ola a e b?
+ // não entendi
+ //explica melhor
+ // eu achei que o retorno seria 1,2 
+ //desculpa, é que o alexandre ta falando aqui
+ // certo, 
+ //vou explicar
+ //exemplo(1,2) isso é chamada da função certo?
+ //o resultado dessa chamada vai pra variavel ola
+ //agora me diga qual o resultado dessa chamada, lembrando que o resultado de uma chmada é o retorno da funcão
+ //certo?sim ah perai não to etendo mais nada foi mal to estressada já
+ //kkkkkk quando eu finalmente desestressei
+ //eu me estressei com seu estresse, agora paciencia 
+ //me diga
+ //qual o retorno da função
+ //1,2, não iane, isso ai são os parametros
+ //a soma 
+ 
+ //entao iane, vc so ta chutando
+ //eu colocquei o nome da função somar de proposito
+ //mas ela não faz nada
+ //vc consegue ver que a função nao tem anda no corpo?
+ //sim
+ // qual o comando pra uma função retornar um valor? return,
+ //exato, en quando uma fuãno ção naéo tem return, qual é o valor que ela retorna?nada
+//não tem esse negocio de nada, tem um valor especifico, ja discutimos
+// undefined
+//isso, entao a funcaao soma retorna undefined, esse valor é guarado na variavel ola
+//compreendeu?
+//sim,compreendi
+//agora me diz o valor da variavel ola?
+//15
+//certo
+//agora me diga o valor da variavel ola
+//calma ai, kkkkkkkk
+// dá true,  retorna o if
+// bora la 
+//vc fez a condição e deu true certo?sim
+//entao entra no if ou no else?
+ //no if 
+ //no if eu executo o comando return 
+ // esse comando ta retornando o que?
+ // ele é aposentado
+ //entao esse é o retorno da função, entenddeu sim
+ //mudei o valor do parametro, qual é o retorno agora
+// da false, entra no else 
+//retorna ' ele não é aposentado'
+//executa no terminal pra ver
+//rodar o codigo
+// sim mas tem que colocar o console log certo? vdd
+
+//ola ai, é
+//  o que falei
+//dentro da função vc pode colocar qualquer comando
+// e retorna o que ta no if e else 
+//dependendo do que eu passar nos parametros, as chamadas podem fazer entrar no if ou no else
+ function somar(idade, sexo) { 
+    if(idade > 60 && sexo == 'masculino') {
+        return 'ele é aposentado'
+    } else {
+    
+        return 'ele não é aposentado'
+    }
+ }
+
+ //so fazer os testes que vc vai ver
+ // deu pra compreender?sim ,pra que vc ta usando outra palavra pra perguntar se eu entendi?
+ //é que vc disse que nao gosta da palavra entendeu
+ // ainda ta brava?
+ //tô, mas continua
+ // caracas mozona, ate eu ja me acalmei
+ // tava com outra espectativa hj
+ // relaxa, a culpa não foi sua, foi dessa porcaria dessa situação
+ // a porcaria do discord veio da pau logo hj , ai como sempre acabei descontando em vc
+ //fica em paz, mas a culpa foi minha tbm , bom continua
+
+ //não foi, eu que fui um cavalo, vc nao teve culpa de nada, vc esta aprendendo
+ //eu que nao tenho paciencia com anda e me estoro por qualquer coisa
+ // isso tbm me estressou tanto que esquece...
+ 
+ //kkk, continua, pode botar pra fora
+ //não era pra gente nem ta falndo disso agora ,deixa quieto
+ //kkkkk, me desculpe mas gaor eu to rindo demais kkkkk
+ /// bom , tais ai ne 
+ //to
+ // bom acho que por hj é isso
+ // ainda sou um desastre ensinando 
+ // mas acredito que um dia eu chego la
+ //eu tbm acredito , mas ue odioooo
+ // ai o ta vendo to sem paciencia ate pra escrever 
+ // bom agente descobriu que quando um fica com raiva, o outro fica tb
+ //pq vc ta com raiva de novo?
+ //agora não, to diznedo que vc ficou com raiva, pq eu fiquei
+ // sim pq na minha cabeça tenho culpa nisso 
+ // nossa eu fico com tanta raiva da minha imcopetencia, que sei la da vontade de nem conversar com as pessoas mais
+ //vc ta em um processo ainda eu tbm tenho que saber lidar com isso e vc não é incopetente
+ //mesmo que eu esteja em um processo não justifica, eu trato mal vc e tals
+ // eu fico com raiva tbm , mas eu entendo fica em paz vc não faz isso de proposito
+ // como vc esta?
+ //ia falara mais aguma coisa colocou uma,ata
+ // eu ja falei,mas to bem ]
+ // kkkkkk, agente ta conversando aqui kkkkkk
+ //pois é ainda bem que isso não executa
+ // cheia de piadinha ne
+ //sea agente tivesse casado , eu sei que eu ia tirar sua raiva
+ //que isso garoto, agora vc queria que executasse ne
+ //kkkkkkkkkkkkkkk
+ //kkkkkkkkk, eu gosto é assim,
+ //mas sério foi mal, me perdoe, eu ainda não sei lidar quando as coisas nãoa contecem como planejei
+ // a questão é que agente colocou muita expecctatpv tbm tava pensando uma coisa e no final saiu outra
+ //pode ser com relação que agente pensava que ia se comunicar bem essas coisas, mas pelo seu aprendizado em si, nao achei que 
+ //foi ruim, so que eu dou voltas demais pra ensinar algo simples, pensando que estou ajudando, ai vc acaba se atrapalhando
+ // relaxa, vamos arrumando isso com tempo
+ //certo, bom , vamos indo, ficou aqule gostinho de nao quero mais kkkkk
+ //mas agente encerra este assunto por aqui, nao vamos ficar comentando dessas coisas foi o nosso acordo
+ //talvez a gente revise no sabado mesmo, já que hj não foi tanto produtivo
+ //não
+ //sabado, é o nosso dia, não vou voltar atraz
+ //eu tava estressado tb, pq era o unico dia na semana que eu poderia ver essas coisas com vc
+ //e nao to arrependido, nao vou voltar atraz na minha apalavra, na smena que vem agente ja começa desse jeito
+ // ta certo então
+ //esqueci de tomar o remedio
+ //nossa foi mal
+ //oxi , foi mal pq ?quem esqueceu foi eu
+ //mas vc esqueceu pq eu deixei vc estressada
+ // ah cara ,para com isso não to mais tão estressada passou ja 
+ //essa conversinha aqui
+ //me ajudou a relaxar
+ //vamos ter que conversar por aqui então kkkk
+ 
+ //kkkkkkk que que isso
+ //rapazoa, vamos ter que orar bastante hein
+ //pq s no namoro agente ta assim, imagina no casamento
+ //eu tenho que mudar rapido hein, imagina agente casado tendo que conversar por aqui
+ //não se escandalize se isso acontecer / ta repreendido 
+ //kkkkkkkk
+ //sai fora, agente vai se dar muito bem isso sim
+ // veradade isso serve pra gente ir pensando em algumas atitudes tbm pra não continuar quando casarmos 
+ //vdd estamos nos conhecendo melhor agora entao
+ // na vdd é bom sabia
+ //minha tia casou com um cara que ele se comportava de um jeito antes de casar
+ //e depois que casou de outro jeito
+ //resltado , eles moram separados uns 3 anos ou mais 
+ //caraca, pois então 
+ // vc ja sabe como eusou na pratica
+ // como diria o filosofo
+ //pior que ta nao fica
+ // eu espero que não fique mesmo kkkkkk
+ //mas to aprendendo a lidar com essas coisas tbm mesmo que não pareça 
+ // eu creio que vc ja conheceio os meus piores lados, ou a maior parte
+ // é por isso que eu tenho certeza que te amo 
+ // eu tb te amo mozona, apesar de todas as merdas que falo
+ // fica em paz temos muito oq melhorar ainda falo isso pq eu tbm tenho que melhorar em muita coisa 
+ // comparado a mim, é bem pouco, mas vai dar certo se Deus quiser
+ // já deu 
+ //bom ta ficando tarde
+ //vdd vamos inddo
+ //Svou me despedir pelo zap kkkkkk
+ //depoiis manda as coisas pro repositorio
+ // na proxima vez será mais produtivo ,vamos tomar iso como um teste 
+ //ok
+ //sim
+const ola = somar(65, 'masculino') //aqui entra no if, e portanto retorna ele é posentado
+const ola2 = somar(35, 'feminino') //ja aqui entra no else e retorna ele nao é aposentado
+const ola3 = somar(70, 'masculino')
+console.log(ola)
+console.log(ola2)
+console.log(ola3)
+
+
---
+++ readme ex.md
@@ -0,0 +1,8 @@
+## calcular resto da divisão
+
+ex : divide 97/6
+16.16666666666
+parte inteira 16 e multiplica por 6
+resultado 96
+97- 96 
+resto da visão (%) 1
